7bb90eba30958071add3ad80bf809f1d144eb7b8
TomoyaIkarashi <tomoya.ikarashi@luxiar.com>
2017-07-03 18:00:47 +0900

    チュートリアル14.2.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+            foreign_key: "follower_id",
+            dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum:50 }
   Valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -17,4 +26,19 @@ class User < ActiveRecord::Base
   def feed
     Micropost.where("user_id=?",id)
   end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  private
+
 end
